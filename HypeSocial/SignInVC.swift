9fc609147b80640b4b1fbd2c900fa18407442ee0
Amir Bahadori <pbahadori156@gmail.com>
2016-09-20 19:17:34 -0700

    Setup Login UI Complete

--- HypeSocial/SignInVC.swift
+++ HypeSocial/SignInVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInVC.swift
 //  HypeSocial
 //
 //  Created by Pasha Bahadori on 9/20/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignInVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
