8d2babdde0eeec7cf5126d7af1aeeee8a1caf9b6
Amir Bahadori <pbahadori156@gmail.com>
2016-10-02 17:25:09 -0700

    Added UIImagePicker to image selector

--- HypeSocial/CircleView.swift
+++ HypeSocial/CircleView.swift
@@ -22,7 +22,7 @@ class CircleView: UIImageView {
     override func layoutSubviews() {
       super.layoutSubviews()
         layer.cornerRadius = self.frame.width / 2
-        
+        clipsToBounds = true
         
     }
 }
--- HypeSocial/FeedVC.swift
+++ HypeSocial/FeedVC.swift
@@ -12,16 +12,22 @@ import FBSDKLoginKit
 import Firebase
 import SwiftKeychainWrapper
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageAdd: CircleView!
     
     var posts = [Post]()
+    var imagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
         
         // Initializes the listener when this view loads in memory - listens out for changes
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
@@ -41,6 +47,25 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
         })  /* End of Snapshot Observe*/
         
     } /* End of ViewDidLoad*/
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //If the image is the image that the user is editing then .. Set the image of our imageAdd button to the edited image
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            imageAdd.image = image
+        } else {
+            print("PASH: A valid image wasn't selected")
+        }
+        //Once we've selected an image, dismiss the picker
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addImageTapped(_ sender: AnyObject) {
+       
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
 
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
